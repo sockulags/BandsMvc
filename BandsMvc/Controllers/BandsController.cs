d884dbcd45b11b1dd33cc44039786096005ff07f
lucas <lucas@DESKTOP-1ETI925>
2023-03-03 14:31:48 +0100

    2a,b,c

---
+++ BandsMvc/Controllers/BandsController.cs
@@ -0,0 +1,22 @@
+﻿using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace acme.Controllers
+{
+    public class BandsController : Controller
+    {
+        DataService dataService = new DataService();
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(dataService.GetAllBands());
+        }
+
+        [HttpGet("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
--- BandsMvc/Models/DataService.cs
+++ BandsMvc/Models/DataService.cs
@@ -2,6 +2,21 @@
 {
     public class DataService
     {
+        List<Band> bands = new List<Band>
+        {
+            new Band {Name = "Kreator", Id=1, Description="Det tyska trashmetalbandet"},
+            new Band {Name = "Metallica", Id=23, Description="Metalbandet"},
+            new Band {Name = "Offspring", Id=34, Description="Punk"}
+        };
 
+        public Band? GetBandById(int id)
+        {
+            return bands.FirstOrDefault(x=>x.Id == id);
+        }
+
+        public Band[] GetAllBands()
+        {
+            return bands.ToArray();
+        }
     }
 }
