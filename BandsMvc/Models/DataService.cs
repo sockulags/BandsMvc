8f4993e617d01b62ed518da2de0f2f7972992810
lucas <lucas@DESKTOP-1ETI925>
2023-03-03 15:52:50 +0100

    -

--- BandsMvc/Models/DataService.cs
+++ BandsMvc/Models/DataService.cs
@@ -1,17 +1,69 @@
-﻿namespace BandsMvc.Models
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using System.Diagnostics.Metrics;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace BandsMvc.Models
 {
     public class DataService
     {
         List<Band> bands = new List<Band>
         {
-            new Band {Name = "Kreator", Id=1, Description="Det tyska trashmetalbandet", ImagePath="/Assets/kreator-2022.jpg"},
-            new Band {Name = "Metallica", Id=23, Description="Metalbandet", ImagePath="/Assets/metallica.jpg"},
-            new Band {Name = "Offspring", Id=34, Description="Punk", ImagePath = "/Assets/offspring.jpg"}
+            new Band {Name = "Kreator", Id=1, Description="Det tyska trashmetalbandet", ImagePath="/Assets/kreator-2022.jpg",
+                Albums=new List<string>{
+                    "Endless Pain (1985)",
+                    "Pleasure to Kill (1986)",
+                    "Terrible Certainty (1987)",
+                    "Extreme Aggression (1989)",
+                    "Coma of Souls (1990)",
+                    "Renewal (1992)",
+                    "Cause for Conflict (1995)",
+                    "Outcast (1997)",
+                    "Endorama (1999)",
+                    "Violent Revolution (2001)",
+                    "Enemy of God (2005)",
+                    "Hordes of Chaos (2009)",
+                    "Phantom Antichrist (2012)",
+                    "Gods of Violence (2017)",
+                    "Hate Über Alles (2022)}"}  },
+            new Band {Name = "Metallica", Id=23, Description="Metalbandet", ImagePath="/Assets/metallica.jpg",
+                Albums=new List<string>{
+                "Kill 'Em All (1983)",
+                "Ride the Lightning (1984)",
+                "Master of Puppets (1986)",
+                "...And Justice for All (1988)",
+                "Metallica (1991)",
+                "Load (1996)",
+                "Reload (1997)",
+                "St. Anger (2003)",
+                "Death Magnetic (2008)",
+                "Hardwired... to Self-Destruct (2016)",
+                "72 Seasons (2023)"
+                } },
+            new Band {Name = "The Offspring", Id=34, Description="Punk", ImagePath = "/Assets/offspring.jpg",
+                Albums=new List<string>{
+                "1989 – The Offspring",
+                "1992 – Ignition",
+                "1994 – Smash",
+                "1997 – Ixnay on the Hombre",
+                "1998 – Americana",
+                "2000 – Conspiracy of One",
+                "2003 – Splinter",
+                "2008 – Rise and Fall, Rage and Grace",
+                "2012 – Days Go By",
+                "2021 – Let the Bad Times Roll"
+                } }
         };
+
+
+
 
         public Band? GetBandById(int id)
         {
-            return bands.FirstOrDefault(x=>x.Id == id);
+            return bands.FirstOrDefault(x => x.Id == id);
         }
 
         public Band[] GetAllBands()
